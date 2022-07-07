14b0291a69f3258c4b74609817d679130854f240
brittneypostma <bdesignedwebdev@gmail.com>
2022-07-07 11:35:42 -0400

    init with 65

---
+++ .netlify/functions-internal/render.js
@@ -0,0 +1,32 @@
+const { init } = require('../serverless.js');
+
+exports.handler = init({
+	appDir: "_app",
+	assets: new Set(["favicon.png","robots.txt","svelte-welcome.png","svelte-welcome.webp"]),
+	mimeTypes: {".png":"image/png",".txt":"text/plain",".webp":"image/webp"},
+	_: {
+		entry: {"file":"start-8ce72083.js","imports":["start-8ce72083.js","chunks/index-9fe72fd8.js","chunks/index-92d65ea7.js","chunks/singletons-d1fb5791.js"],"stylesheets":[]},
+		nodes: [
+			() => Promise.resolve().then(() => require('../server/nodes/0.js')),
+			() => Promise.resolve().then(() => require('../server/nodes/1.js')),
+			() => Promise.resolve().then(() => require('../server/nodes/4.js'))
+		],
+		routes: [
+			{
+				type: 'page',
+				id: "todos",
+				pattern: /^\/todos\/?$/,
+				names: [],
+				types: [],
+				path: "/todos",
+				shadow: () => Promise.resolve().then(() => require('../server/entries/endpoints/todos/index.js')),
+				a: [0,2],
+				b: [1]
+			}
+		],
+		matchers: async () => {
+			
+			return {  };
+		}
+	}
+});
